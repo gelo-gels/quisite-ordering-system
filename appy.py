dc1a6d576dc0c2a1ffe0d5e37d6337cddbac5d9d
Codetech-bit <riangeloo17@gmail.com>
2024-05-12 01:33:40 +0800

    push for production

--- appy.py
+++ appy.py
@@ -3,4 +3,4 @@ from src import init_db
 app = init_db()
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=7000, debug=True, threaded=False)
+    app.run('0.0.0.0', port=7000, debug=False, threaded=False)
